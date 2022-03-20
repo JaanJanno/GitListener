9f90b154984af5b66a6b08277ccf3ee8d4aa7994
Jaan Janno <jaan911@gmail.com>
2022-03-20 16:12:24 +0200

    Update

--- listener.py
+++ listener.py
@@ -21,8 +21,8 @@ def cleanall():
     data = dict(request.headers)
     if not 'Token' in data or data['Token'] != token:
         return {'status': 'Unauthorized'}, 401
-    subprocess.run(['rm', '-rf', os.path.join(dagsfolder)])
-    subprocess.run(['mkdir', dagsfolder])
+    subprocess.run(['rm', '-rf', os.path.join(dagsfolder, '*')])
+    subprocess.run(['rm', '-rf', os.path.join(dagsfolder, '.*')])
     return {'status': 'OK'}, 200
 
 @app.route('/connect', methods=['POST'])
